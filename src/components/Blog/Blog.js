0adfe9a1489e1b146d91e75121984828504a295f
Mahedi-junior <mahedih.cse@gmail.com>
2022-11-22 13:19:44 +0600

    blog completed

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -5,34 +5,66 @@ const Blog = () => {
   return (
     <div className="p-3">
       <h1 className="">React Important Question</h1>
-      <Container className="mt-5 fluid px-5">
+      <Container className="mt-5 px-5">
         <Accordion defaultActiveKey="0" flush>
           <Accordion.Item eventKey="0">
             <Accordion.Header>
-              <p className="fw-bold ">
+              <p className="fw-bold text-success ">
                 #1. What is the purpose of react router?
               </p>
             </Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              React Router, and dynamic, client-side routing, allows us to build
+              a single-page web application with navigation without the page
+              refreshing as the user navigates. React Router uses component
+              structure to call components, which display the appropriate
+              information.
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1">
-            <Accordion.Header>Accordion Item #2</Accordion.Header>
+            <Accordion.Header>
+              <p className="fw-bold text-success">
+                #2..How does context api work ?
+              </p>
+            </Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              <p>
+                Context is a built-in API introduced in ​​React 16.3. It makes
+                it possible to pass data from parent to children nested deep
+                down the component tree directly, instead of passing it down
+                through a chain of props
+              </p>
+              <p>
+                A context contains references to variables and notifies its
+                consumers when these references are changed. There are no
+                restrictions on what type of values you can provide, so you can
+                easily pass in functions that can be used to communicate events
+                back from consumers, for example.
+              </p>
+              <p>
+                To create a context, call the createContext function with an
+                arbitrary value. This will be the default value provided to the
+                consumers. Optionally provide an interface describing your
+                provided object. The returned object will contain helper
+                components you need to provide or consume this context.
+              </p>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>
+              <p className="fw-bold text-success">#3..Explain useRef Hook</p>
+            </Accordion.Header>
+            <Accordion.Body>
+              <div className="">
+                <p className="fw-bold">
+                  useRef is a React Hook that lets you reference a value that’s
+                  not needed for rendering.
+                </p>
+                <strong>Usage of useRef Hook </strong>
+                <p>Referencing a value with a ref</p>
+                <p>Manipulating the DOM with a ref</p>
+                <p>Avoiding recreating the ref contents</p>
+              </div>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
